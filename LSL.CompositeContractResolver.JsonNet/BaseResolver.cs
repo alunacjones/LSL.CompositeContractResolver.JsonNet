67798e96a2f5281c5fde025f4d089fcac0413f82
Alun Jones <alunacjones@gmail.com>
2021-05-23 01:51:16 +0100

    Fix issue for now - but revisit later. Internal design issue that needs to be improved

---
+++ LSL.CompositeContractResolver.JsonNet/BaseResolver.cs
@@ -0,0 +1,9 @@
+namespace LSL.CompositeContractResolver.JsonNet
+{
+    /// <summary>
+    /// All resolvers should inherit from this class and implement all the I
+    /// </summary>
+    public abstract class BaseResolver
+    {
+    }
+}
